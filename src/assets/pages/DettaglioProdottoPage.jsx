e327f89f7f5057f3daa4d94d251016a57c73f88e
SergioGBosco <sergiogibosco@gmail.com>
2025-09-08 18:17:02 +0200

    feat:  use useNavigate to navigate through the products

--- src/assets/pages/DettaglioProdottoPage.jsx
+++ src/assets/pages/DettaglioProdottoPage.jsx
@@ -1,22 +1,46 @@
 import axios from "axios";
-import { use, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 const DettaglioProdottoPage = () => {
   const { id } = useParams();
-
   const [details, setDetails] = useState([])
+  const navigate = useNavigate();
 
 
   useEffect(() => {
-    axios.get(`https://fakestoreapi.com/products/:${id}`).then(resp => {
+    axios.get(`https://fakestoreapi.com/products/${id}`).then(resp => {
       setDetails(resp.data)
-      console.log(details)
     })
-  }, [])
+  }, [id]);
 
   return (
     <div>
-      <h1>hai selezionato il prodotto con id : {id}</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="d-flex justify-content-center">
+
+              <div className="card card-detail" >
+                <img src={details.image} className="card-img-top" alt={details.title} />
+                <div className="card-body">
+                  <p className="card-text">{details.price}.</p>
+                  <p className="card-text">{details.title}.</p>
+                  <p className="card-text">{details.category}.</p>
+                  <p className="card-text">{details.description}.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-12">
+              <div className="d-flex m-4 justify-content-around">
+                <button className="btn btn-primary" onClick={() => navigate(`/prodotti/${parseInt(id) - 1}`)}> torna alla pagina precedente </button>
+                <button className="btn btn-success" onClick={() => navigate(`/prodotti/${parseInt(id) + 1}`)}> Vai Alla pagina Successiva </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
